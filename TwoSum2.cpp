7c9ecefa242607442845ff7f3e841af59ef2570f
Vikas Yadav <vikasvy369@gmail.com>
2025-11-11 01:06:18 +0530

    Two Sum II

---
+++ TwoSum2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+    vector<int> twoSum(vector<int>& numbers, int target) {
+            int n=numbers.size();
+            int i=0;
+            int j=n-1;
+            while(i<j){
+            int sum=numbers[i]+numbers[j];
+            if(sum==target){
+                return {i+1,j+1};
+            }
+            else if(sum < target){
+                i++;
+            }
+            else{
+                j--;
+            }
+            }
+        return {};
+    }
+int main(){
+    vector<int> numbers = {2,7,11,15};
+    int target;
+    cin>>target;
+    vector<int> result = twoSum(numbers,target);
+    cout << result[0] << " " << result[1] << endl;
+    return 0;
+
+}
