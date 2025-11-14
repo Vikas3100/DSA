4ed1129ca9baf63fd73e3be75748bee6d1db7ff7
Vikas Yadav <vikasvy369@gmail.com>
2025-11-14 23:57:43 +0530

    Buy and Sell Problem 1

---
+++ buySell1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i < prices.size(); i++){
+            int cost = prices[i] - mini;
+            maxProfit = max(maxProfit, cost);
+            mini = min(mini, prices[i]);
+        }
+        return maxProfit;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> prices(n);
+    for(int i = 0; i < n; i++) cin >> prices[i];
+    Solution s;
+    cout << s.maxProfit(prices);
+}
