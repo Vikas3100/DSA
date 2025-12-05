93d61362089860efeab80e46a7168b45daca1ea7
Vikas Yadav <vikasvy369@gmail.com>
2025-12-05 23:19:02 +0530

    updated

--- basic_recursion.cpp
+++ basic_recursion.cpp
@@ -14,6 +14,33 @@
 // }
 #include<bits/stdc++.h>
 using namespace std;
+// void functionName(int i,int n){
+//     if(i>n){
+//         return;
+//     }
+//     cout<<"Vikas"<<endl;
+//     functionName(i+1,n++);
+    
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     functionName(1,n);
+// }
+// void rev( int n){
+//     if(n==0){
+//         return;
+//     }
+//     cout<<n;
+//     rev(--n);
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     rev(n);
+// }
+#include<bits/stdc++.h>
+using namespace std;
 int fun(int n){
     if(n==0){
         return 0;
