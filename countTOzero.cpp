0f4fec3ffa4e0ab83835225fd06db6d2c588c0eb
Vikas Yadav <vikasvy369@gmail.com>
2025-12-06 18:33:04 +0530

    updated count

--- countTOzero.cpp
+++ countTOzero.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
     int countOperations(int num1, int num2) {
+
+
+
+        int m;
         int count=0;
         while(num1 && num2){
             if(num1>=num2){
